24efdb7264a71c8976bc0a4a463aaef110206b68
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-04-25 22:17:12 +0200

    Add counsel

--- emacs.d/lisp/bahnhof-ivy.el
+++ emacs.d/lisp/bahnhof-ivy.el
@@ -1,8 +1,7 @@
 ;;; bahnhof-ivy.el                                    -*- lexical-binding: t -*-
 
 (use-package ivy
-  :demand  ; otherwise find-file and similar will not work
-  :diminish
+  :demand
 
   :bind (:map ivy-minibuffer-map
          ("C-l" . ivy-alt-done)
@@ -16,6 +15,15 @@
          ("C-d" . ivy-switch-buffer-kill))
 
   :config (ivy-mode 1))
+
+(use-package counsel
+  :demand
+
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file))
+
+  :config (setq ivy-initial-inputs-alist nil)) ;; Don't search starting with ^
 
 
 (provide 'bahnhof-ivy)
