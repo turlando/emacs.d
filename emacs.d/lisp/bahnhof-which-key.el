ae65a69331239197068ec5f8b5c8a08eb939dd0b
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-04-27 21:06:46 +0200

    Add which-key

---
+++ emacs.d/lisp/bahnhof-which-key.el
@@ -0,0 +1,11 @@
+;;; bahnhof-which-key.el                              -*- lexical-binding: t -*-
+
+(use-package which-key
+  :requires evil
+  :delight
+  :init (which-key-mode)
+  :config (setq which-key-idle-delay 0.3))
+
+
+(provide 'bahnhof-which-key)
+;;; bahnhof-which-key.el ends here
