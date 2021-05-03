5bb77e1e3a93e197e41cd9449c9a37feeea3a9c7
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-05-03 09:49:51 +0200

    Add indent guides and enable them for YAML

--- emacs.d/lisp/bahnhof-programming.el
+++ emacs.d/lisp/bahnhof-programming.el
@@ -3,6 +3,13 @@
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
+(use-package highlight-indent-guides
+  :delight
+
+  :custom
+  (highlight-indent-guides-responsive 'top)
+  (highlight-indent-guides-method     'bitmap))
+
 
 ;; Automatically balance parens
 (add-hook 'prog-mode-hook 'electric-pair-mode)
--- emacs.d/lisp/bahnhof-yaml.el
+++ emacs.d/lisp/bahnhof-yaml.el
@@ -1,6 +1,7 @@
 ;;; bahnhof-yaml.el                                   -*- lexical-binding: t -*-
 
-(use-package yaml-mode)
+(use-package yaml-mode
+  :hook (yaml-mode . highlight-indent-guides-mode))
 
 
 (provide 'bahnhof-yaml)
