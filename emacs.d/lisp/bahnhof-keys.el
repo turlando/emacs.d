de1c3eee8ede1be0a37879303e272ef65864f5a9
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-05-04 22:56:13 +0200

    Add more window and buffer keybinds

--- emacs.d/lisp/bahnhof-keys.el
+++ emacs.d/lisp/bahnhof-keys.el
@@ -1,13 +1,23 @@
 ;;; bahnhof-keys.el                                   -*- lexical-binding: t -*-
 
+;;; Prefix and infix keys
+
 (defconst bahnhof-leader-key "SPC")
 (defconst bahnhof-buffer-key "b")
+(defconst bahnhof-window-key "w")
+
+
+;;; Packages
 
 (use-package hercules
   :demand)
 
 (use-package general
-  :demand)
+  :demand
+  :requires (evil hercules))
+
+
+;;; Leader key
 
 (general-create-definer bahnhof-leader-def
   :states '(emacs normal motion insert visual hybrid operator)
@@ -16,6 +26,9 @@
 
 (bahnhof-leader-def
  "SPC" '(counsel-M-x :which-key "M-x"))
+
+
+;;; Buffer key
 
 (general-create-definer bahnhof-buffer-def
   :wrapping bahnhof-leader-def
@@ -37,11 +50,59 @@
 
 (bahnhof-buffer-def
   "b" '(counsel-switch-buffer           :which-key "switch")
-  "f" '(bahnhof-toggle-buffer-flip-mode :which-key "flip")
-  "n" '(next-buffer                     :which-key "next")
-  "p" '(previous-buffer                 :which-key "prev")
   "k" '(kill-buffer                     :which-key "kill")
-  "d" '(kill-buffer-and-window          :which-key "kill-close"))
+  "d" '(kill-buffer-and-window          :which-key "kill-close")
+  "f" '(bahnhof-toggle-buffer-flip-mode :which-key "flip")
+  "h" '(previous-buffer                 :which-key "prev")
+  "j" '(next-buffer                     :which-key "next")
+  "k" '(previous-buffer                 :which-key "prev")
+  "l" '(next-buffer                     :which-key "next"))
+
+
+;;; Window key
+
+(general-create-definer bahnhof-window-def
+  :wrapping bahnhof-leader-def
+  :infix bahnhof-window-key
+  :wk-full-keys nil
+  "" '(:ignore t :which-key "window"))
+
+(general-def
+  :prefix-map 'bahnhof-window-move-map
+  "h" '(evil-window-left  :which-key "left")
+  "j" '(evil-window-down  :which-key "down")
+  "k" '(evil-window-up    :which-key "up")
+  "l" '(evil-window-right :which-key "right")
+  "w" '(evil-window-next  :which-key  "next"))
+
+(hercules-def
+  :toggle-funs 'bahnhof-toggle-window-move-mode
+  :keymap 'bahnhof-window-move-map
+  :transient t)
+
+(general-def
+  :prefix-map 'bahnhof-window-resize-map
+  "h" '(shrink-window-horizontally  :which-key "shrink-h")
+  "j" '(enlarge-window              :which-key "enlarge-v")
+  "k" '(shrink-window               :which-key "shrink-v")
+  "l" '(enlarge-window-horizontally :which-key "enlarge-h"))
+
+(hercules-def
+ :toggle-funs 'bahnhof-toggle-window-resize-mode
+ :keymap 'bahnhof-window-resize-map
+ :transient t)
+
+(bahnhof-window-def
+  "m" '(bahnhof-toggle-window-move-mode   :which-key "move")
+  "h" '(evil-window-left                  :which-key "left")
+  "j" '(evil-window-down                  :which-key "down")
+  "k" '(evil-window-up                    :which-key "up")
+  "l" '(evil-window-right                 :which-key "right")
+  "w" '(evil-window-next                  :which-key "next")
+  "v" '(split-window-vertically           :which-key "split-v")
+  "b" '(split-window-horizontally         :which-key "split-h")
+  "r" '(bahnhof-toggle-window-resize-mode :which-key "resize")
+  "d" '(delete-window                     :which-key "close"))
 
 
 (provide 'bahnhof-keys)
