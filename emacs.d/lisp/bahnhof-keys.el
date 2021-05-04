0c0b210a1e4ec9862a9b54be272e966494c55713
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-05-04 17:49:37 +0200

    Add flip buffer transient mode

--- emacs.d/lisp/bahnhof-keys.el
+++ emacs.d/lisp/bahnhof-keys.el
@@ -3,6 +3,9 @@
 (defconst bahnhof-leader-key "SPC")
 (defconst bahnhof-buffer-key "b")
 
+(use-package hercules
+  :demand)
+
 (use-package general
   :demand
 
@@ -21,8 +24,21 @@
   (bahnhof-leader-def
    "SPC" '(counsel-M-x :which-key "M-x"))
 
+  (general-def
+   :prefix-map 'bahnhof-buffer-flip-map
+   "h" 'previous-buffer
+   "j" 'next-buffer
+   "k" 'previous-buffer
+   "l" 'next-buffer)
+
+  (hercules-def
+   :toggle-funs 'bahnhof-toggle-buffer-flip-mode
+   :keymap 'bahnhof-buffer-flip-map
+   :transient t)
+
   (bahnhof-buffer-def
    "b" 'counsel-switch-buffer
+   "f" '(bahnhof-toggle-buffer-flip-mode :which-key "flip")
    "n" 'next-buffer
    "p" 'previous-buffer
    "k" 'kill-buffer
