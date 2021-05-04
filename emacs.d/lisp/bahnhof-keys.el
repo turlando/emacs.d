2320fae2cd8a45b8e1c047730f7edf94055aae58
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-05-04 17:52:12 +0200

    Move general and hercules config outside of use-package

--- emacs.d/lisp/bahnhof-keys.el
+++ emacs.d/lisp/bahnhof-keys.el
@@ -7,42 +7,41 @@
   :demand)
 
 (use-package general
-  :demand
+  :demand)
 
-  :config
-  (general-create-definer bahnhof-leader-def
-    :states '(emacs normal motion insert visual hybrid operator)
-    :prefix bahnhof-leader-key
-    :keymaps 'override)
+(general-create-definer bahnhof-leader-def
+  :states '(emacs normal motion insert visual hybrid operator)
+  :prefix bahnhof-leader-key
+  :keymaps 'override)
 
-  (general-create-definer bahnhof-buffer-def
-    :wrapping bahnhof-leader-def
-    :infix bahnhof-buffer-key
-    :wk-full-keys nil
-    "" '(:ignore t :which-key "buffer"))
+(bahnhof-leader-def
+ "SPC" '(counsel-M-x :which-key "M-x"))
 
-  (bahnhof-leader-def
-   "SPC" '(counsel-M-x :which-key "M-x"))
+(general-create-definer bahnhof-buffer-def
+  :wrapping bahnhof-leader-def
+  :infix bahnhof-buffer-key
+  :wk-full-keys nil
+  "" '(:ignore t :which-key "buffer"))
 
-  (general-def
-   :prefix-map 'bahnhof-buffer-flip-map
-   "h" 'previous-buffer
-   "j" 'next-buffer
-   "k" 'previous-buffer
-   "l" 'next-buffer)
+(general-def
+  :prefix-map 'bahnhof-buffer-flip-map
+  "h" 'previous-buffer
+  "j" 'next-buffer
+  "k" 'previous-buffer
+  "l" 'next-buffer)
 
-  (hercules-def
-   :toggle-funs 'bahnhof-toggle-buffer-flip-mode
-   :keymap 'bahnhof-buffer-flip-map
-   :transient t)
+(hercules-def
+  :toggle-funs 'bahnhof-toggle-buffer-flip-mode
+  :keymap 'bahnhof-buffer-flip-map
+  :transient t)
 
-  (bahnhof-buffer-def
-   "b" 'counsel-switch-buffer
-   "f" '(bahnhof-toggle-buffer-flip-mode :which-key "flip")
-   "n" 'next-buffer
-   "p" 'previous-buffer
-   "k" 'kill-buffer
-   "d" 'kill-buffer-and-window))
+(bahnhof-buffer-def
+  "b" 'counsel-switch-buffer
+  "f" '(bahnhof-toggle-buffer-flip-mode :which-key "flip")
+  "n" 'next-buffer
+  "p" 'previous-buffer
+  "k" 'kill-buffer
+  "d" 'kill-buffer-and-window)
 
 
 (provide 'bahnhof-keys)
