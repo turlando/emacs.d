2ec43db14168360ad2b096dcc664a8eda25c7ba8
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-06-01 17:13:22 +0200

    Fix broken redo

--- emacs.d/lisp/bahnhof-evil.el
+++ emacs.d/lisp/bahnhof-evil.el
@@ -1,11 +1,14 @@
 ;;; bahnhof-evil.el                                   -*- lexical-binding: t -*-
 
+(use-package undo-fu)
+
 (use-package evil
   :demand
 
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
+  (setq evil-undo-system 'undo-fu)
 
   :config
   (evil-mode 1))
